bef626520ef1cd5fcd33ea69ce379d8b90d77bfe
Atta ul Qayyum - 33274 <locadmin@PKKHIHOITDN651.baf.com>
2021-12-08 10:18:56 +0500

    new native

---
+++ target/quarkus-grpc2095243568232694721.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Java\jdk-11.0.12\bin\java.EXE" -cp "C:\Users\locadmin\.m2\repository\io\quarkus\quarkus-grpc-protoc-plugin\2.5.0.Final\quarkus-grpc-protoc-plugin-2.5.0.Final-shaded.jar" io.quarkus.grpc.protoc.plugin.MutinyGrpcGenerator
